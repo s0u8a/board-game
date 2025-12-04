f513b1a03f394494496b93629dec6e229179f3a7
takee <koukigaa@gmail.com>
2025-12-04 14:07:17 +0900

    フォーム修正

--- login.php
+++ login.php
@@ -12,22 +12,19 @@
     <header>
         <h1>ボードゲームカフェ</h1>
         <nav>
-            <a class="header-link" href="register.html">会員登録</a>
+            <a class="header-link" href="register.php">会員登録</a>
         </nav>
     </header>
     <main>
         <h1>ログイン</h1>
         <div class="login_form">
             <form action="#" method="post">
-                <p><label for="">メールアドレス</label></p>
-                <p><input type="text"></p>
-                <p><label for="">パスワード</label></p>
-                <p><input type="password"></p>
-
+                <p><label>メールアドレス<br><input type="text"></label></p>
+                <p><label>パスワード<br><input type="password"></label></p>
                 <button>ログイン</button>
             </form>
             <p>アカウントをお持ちでない方はこちら</p>
-            <p><a href="register.html">会員登録</a></p>
+            <p><a href="register.php">会員登録</a></p>
         </div>
     </main>
     <footer>
